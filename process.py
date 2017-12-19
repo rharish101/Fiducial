3ac348bf5c91aca8fe140129c50fd6449178fc0e
rharish101 <harish.rajagopals@gmail.com>
2017-12-19 13:23:18 +0530

    Made code modular

--- process.py
+++ process.py
@@ -4,6 +4,80 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+# Smoothened Image Histogram
+def img_hist(image, filter_sigma=10):
+    hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+    hist = np.reshape(gaussian_filter1d(hist, filter_sigma), (len(hist)))
+    return hist
+
+# Returns "1st" local minima of a function
+def func_minima(func):
+    for i in range(len(func)):
+        if i > 0 and i < (len(func) -1) and func[i] <= func[i - 1] and\
+        func[i] <= func[i + 1]:
+            return i
+
+# Second-derivative of Ratio Curve of Image Histogram: Normalized Rate Curve
+def norm_rate_curve(hist, filter_sigma=2, verbose=False):
+    summation = hist * range(1, 1 + len(hist))
+    ratio = [np.sum(summation[:i]) / np.sum(summation[(i + 1):]) for i in\
+            range(len(summation) - 1)]
+    x = np.arange(len(ratio))
+    if verbose:
+        plt.title('Ratio Curve')
+        plt.plot(x, ratio)
+        plt.show()
+
+    y_first = np.diff(ratio) / np.diff(x)
+    x_first = 0.5 * (x[:-1] + x[1:])
+    y_second = np.diff(y_first) / np.diff(x_first)
+    second_der = gaussian_filter1d(y_second, filter_sigma)
+    if verbose:
+        x_second = 0.5 * (x_first[:-1] + x_first[1:])
+        plt.title('Normalized Rate Curve')
+        plt.plot(x_second, second_der)
+        plt.show()
+    return second_der
+
+# Thresholding of image from 1st minima of histogram
+# NOTE: This only works if background is noticeably darker than the brain
+def thresh_hist(image, hist_filter_sigma=10, thresh_filter_sigma=2,
+                verbose=False):
+    hist = img_hist(image, filter_sigma=hist_filter_sigma)
+    threshold = func_minima(hist)
+    new_img = np.where(image>=threshold, 255 * np.ones(image.shape),
+                       np.zeros(image.shape))
+    new_img = gaussian_filter(new_img, thresh_filter_sigma)
+    thresh_img =  ((new_img / new_img.max()) * 255).astype(np.uint8)
+
+    if verbose:
+        plt.title('Image Histogram')
+        plt.plot(np.arange(len(hist)), hist)
+        plt.plot(threshold, hist[threshold], 'rx')
+        plt.show()
+
+        plt.title('Thresholded Image')
+        plt.imshow(thresh_img, cmap='gray', vmin=0, vmax=255)
+        plt.show()
+
+    return thresh_img
+
+# Canny-Edge detection
+def canny_edge(image, edge_filter_sigma=2, binarize=False, verbose=False,
+               **kwargs):
+    if binarize:
+        image = thresh_hist(image, verbose=verbose, **kwargs)
+    edges = cv2.Canny(image, 100, 200)
+    edges = gaussian_filter(edges, edge_filter_sigma)
+    edges = ((edges / edges.max()) * 255).astype(np.uint8)
+
+    if verbose:
+        plt.title('Image Edges')
+        plt.imshow(edges, cmap='gray', vmin=0, vmax=255)
+        plt.show()
+
+    return edges
+
 img_source = './fiducial.png'
 
 img = cv2.imread(img_source, 0)
@@ -11,57 +85,5 @@ plt.title('Image')
 plt.imshow(img, cmap='gray', vmin=0, vmax=255)
 plt.show()
 
-# Smoothed Image Histogram and it's first minima
-hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-x = np.arange(len(hist))
-smooth_hist = np.reshape(gaussian_filter1d(hist, 10), (len(hist)))
-minima = 0
-for i in x:
-    if i > 0 and i < x[-1] and smooth_hist[i] <= smooth_hist[i - 1] and\
-    smooth_hist[i] <= smooth_hist[i + 1]:
-        minima = i
-        break
-plt.title('Image Histogram')
-plt.plot(x, smooth_hist)
-plt.plot(minima, smooth_hist[minima], 'rx')
-plt.show()
-
-# Thresholding of image from 1st minima of histogram
-# NOTE: This only works if background is noticeably darker than the brain
-new_img = np.where(img>=minima, 255 * np.ones(img.shape), np.zeros(img.shape))
-new_img = gaussian_filter(new_img, 2)
-new_img = ((new_img / new_img.max()) * 255).astype(np.uint8)
-plt.title('Thresholded Image')
-plt.imshow(new_img, cmap='gray', vmin=0, vmax=255)
-plt.show()
-
-# Canny-Edge detection
-edges = cv2.Canny(new_img, 100, 200)
-edges = gaussian_filter(edges, 2)
-edges = ((edges / edges.max()) * 255).astype(np.uint8)
-plt.title('Image Edges')
-plt.imshow(edges, cmap='gray', vmin=0, vmax=255)
-plt.show()
-
-exit()
-
-# Ratio Curve
-summation = smooth_hist * range(1, 1 + len(smooth_hist))
-ratio = [np.sum(summation[:i]) / np.sum(summation[(i + 1):]) for i in\
-         range(len(summation) - 1)]
-x = x[:-1]
-plt.title('Ratio Curve')
-plt.plot(x, ratio)
-plt.show()
-
-# Second-derivative of Normalized Rate Curve
-y_first = np.diff(ratio) / np.diff(x)
-x_first = 0.5 * (x[:-1] + x[1:])
-y_second = np.diff(y_first) / np.diff(x_first)
-x_second = 0.5 * (x_first[:-1] + x_first[1:])
-second_der = gaussian_filter1d(y_second, 2)
-plt.title('Normalized Rate Curve')
-plt.plot(x_second, second_der)
-#plt.imshow(smoothed_thresh, cmap='gray', vmin=0, vmax=255)
-plt.show()
+canny_edge(img, binarize=True, verbose=True)
 
