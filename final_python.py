cc3e280a2d95af7e400c8634780b9d39ef38262c
Vishwas Lathi <lathivishwas@gmail.com>
2018-01-03 03:37:30 +0530

    Update final_python.py

--- final_python.py
+++ final_python.py
@@ -22,19 +22,19 @@ return image
 def template1(plane,index):
 #Boolean is true if image has template1 
 #x,y are the coordinates of the center of concentric circles and sets Ma[x][y][index]=1 is plane='axial'....
-return Boolean,x,y
+return {'Bool':,'x_coor':,'y_coor':}
 
 def template2(plane,index):
 #x,y are the coordinates of the center of the valley
 #returns true if template2 is found and sets Ma[x][y][index]=2 if plane='axial'...    
-return Boolean,x,y
+return {'Bool': ,'x_coor': ,'y_coor': }
 
 #main code starts:
 index=1
 template1('axial',1)=False
 template2('axial',1)=False
 #N is the no. of slices of each view
-while index is not equal to N and template1('axial',index) is not True and template2('axial',index) is not True:
+while index is not equal to N and template1('axial',index)['Bool'] is not True and template2('axial',index)['Bool'] is not True:
  template1('axial',index)
  template2('axial',index)
  index++
@@ -44,7 +44,7 @@ for a in range(x_dim):
     for b in range(y_dim):
         for c in range(z_dim):
             if Ms[a][b][c]==1:
-               if template2('saggital',a) is True and template2('coronal',b) is True:
+               if template2('saggital',a)['Bool'] is True and template2('coronal',b)['Bool'] is True:
                   print 'Fiducial present at (x,y,z)'
                   fidu_coordinates[x][y][z]=1  
 
@@ -52,7 +52,7 @@ for a1 in range(x_dim):
     for b1 in range(y_dim):     
         for c1 in range(z_dim): 
             if Ms[a1][b1][c1]==2:
-               if (template1('saggital',a1) or template2('saggital',a1)) and (template1('saggital',b1) or template2('coronal',b1) is True:
+               if (template1('saggital',a1)['Bool'] or template2('saggital',a1)['Bool']) and (template1('saggital',b1)['Bool'] or template2('coronal',b1)['Bool'] is True:
                   print 'Fiducial present at (x,y,z)'
                   fidu_coordinates[x][y][z]=1
                                                                               
