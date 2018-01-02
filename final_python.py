fbda3e49e1c58f08056e0aa9791c619061ff6973
Vishwas Lathi <lathivishwas@gmail.com>
2018-01-02 21:37:11 +0530

    Add files via upload

---
+++ final_python.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+Ma=np.zeros((512,512),dtype=np.float32)
+Ms=np.zeros((512,512),dtype=np.float32)
+Mc=np.zeros((512,512),dtype=np.float32)
+
+i=1
+#edge detection on I(a,i)
+def edge_detection_axial(i):
+
+return template
+
+if template='template1'
+  #x,y coordinate of the concentric circle
+   Ma[x][y]=1
+   #traversing along the brain boundary
+if edge_type=True
+   while i is not 0:
+      edge_detection_axial(i)
+      i--   
+   while i is not max_axial:
+      edge_detection_axial(i)
+      i++    
+#check if template 2 is found       
+if template2=True:
+   #x,y is the loaction of the valley between the peaks
+   ma[x][y]=2
+if template is not 'template1' and 'template2' and i is not max_axial: 
+   i=i+1
+   #goto1
+
+        
+
+
+  
