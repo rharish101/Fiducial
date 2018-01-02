670e7cb0228e246aea9426c0e99b64271152fde5
Vishwas Lathi <lathivishwas@gmail.com>
2018-01-03 03:19:59 +0530

    Update final_python.py

--- final_python.py
+++ final_python.py
@@ -1,33 +1,67 @@
+#python 2.7
 import numpy as np
 
-Ma=np.zeros((512,512),dtype=np.float32)
-Ms=np.zeros((512,512),dtype=np.float32)
-Mc=np.zeros((512,512),dtype=np.float32)
+#enter the dimensions of the image
+x_dim=
+y_dim=
+z_dim=
 
-i=1
-#edge detection on I(a,i)
-def edge_detection_axial(i):
+Ma=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+Mc=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+Ms=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 
-return template
+fidu_coordinates=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 
-if template='template1'
-  #x,y coordinate of the concentric circle
-   Ma[x][y]=1
-   #traversing along the brain boundary
-if edge_type=True
-   while i is not 0:
-      edge_detection_axial(i)
-      i--   
-   while i is not max_axial:
-      edge_detection_axial(i)
-      i++    
-#check if template 2 is found       
-if template2=True:
-   #x,y is the loaction of the valley between the peaks
-   ma[x][y]=2
-if template is not 'template1' and 'template2' and i is not max_axial: 
-   i=i+1
-   #goto1
+
+#given plane and index of the slice this function outputs the image 
+def image(plane,index):
+
+return image
+
+
+def template1(plane,index):
+#Boolean is true if image has template1 
+#x,y are the coordinates of the center of concentric circles and sets Ma[x][y][index]=1 is plane='axial'....
+return Boolean,x,y
+
+def template2(plane,index):
+#x,y are the coordinates of the center of the valley
+#returns true if template2 is found and sets Ma[x][y][index]=2 if plane='axial'...    
+return Boolean,x,y
+
+#main code starts:
+index=1
+template1('axial',1)=False
+template2('axial',1)=False
+#N is the no. of slices of each view
+while index is not equal to N and template1('axial',index) is not True and template2('axial',index) is not True:
+ template1('axial',index)
+ template2('axial',index)
+ index++
+ 
+#some issues reagarding last if statement
+for a in range(x_dim):
+    for b in range(y_dim):
+        for c in range(z_dim):
+            if Ms[a][b][c]==1:
+               if template2('saggital',a) is True and template2('coronal',b) is True:
+                  print 'Fiducial present at (x,y,z)'
+                  fidu_coordinates[x][y][z]=1  
+
+for a1 in range(x_dim):
+    for b1 in range(y_dim):     
+        for c1 in range(z_dim): 
+            if Ms[a1][b1][c1]==2:
+               if (template1('saggital',a1) or template2('saggital',a1)) and (template1('saggital',b1) or template2('coronal',b1) is True:
+                  print 'Fiducial present at (x,y,z)'
+                  fidu_coordinates[x][y][z]=1
+                                                                              
+
+           
+
+
+
+
 
         
 
