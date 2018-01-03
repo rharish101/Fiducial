84ad113775bcda1ad704e2da04e9ff650b44edb1
Vishwas Lathi <lathivishwas@gmail.com>
2018-01-03 13:42:47 +0530

    Update final_python.py

--- final_python.py
+++ final_python.py
@@ -10,54 +10,63 @@ Ma=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 Mc=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 Ms=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 
-fidu_coordinates=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+
 
 
 #given plane and index of the slice this function outputs the image 
 def image(plane,index):
+    if plane=='axial':
+       image=list_axial[index-1]
+    elif plane=='coronal':
+       image=list_coronal[index-1]
+    else:
+       image=list_saggital[index-1]
 
 return image
 
 
 def template1(plane,index):
+   image=image(plane,index)
 #Boolean is true if image has template1 
 #x,y are the coordinates of the center of concentric circles and sets Ma[x][y][index]=1 is plane='axial'....
 return {'Bool':,'x_coor':,'y_coor':}
 
 def template2(plane,index):
+   image=image(plane,index)
 #x,y are the coordinates of the center of the valley
 #returns true if template2 is found and sets Ma[x][y][index]=2 if plane='axial'...    
 return {'Bool': ,'x_coor': ,'y_coor': }
 
-#main code starts:
-index=1
-template1('axial',1)=False
-template2('axial',1)=False
-#N is the no. of slices of each view
-while index is not equal to N and template1('axial',index)['Bool'] is not True and template2('axial',index)['Bool'] is not True:
- template1('axial',index)
- template2('axial',index)
- index=index+1
- 
-#some issues reagarding last if statement
-for a in range(x_dim):
-    for b in range(y_dim):
-        for c in range(z_dim):
-            if Ms[a][b][c]==1:
-               if template2('saggital',a)['Bool'] is True and template2('coronal',b)['Bool'] is True:
-                  print 'Fiducial present at (x,y,z)'
-                  fidu_coordinates[x][y][z]=1  
+def god_function(list_axial,list_coronal,list_saggital): 
+        list_axial=list_axial 
+        list_coronal=list_coronal 
+        list_saggital=list_saggital
+        fidu_coordinates=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+        #N is the no of slices
+        index=1
+        while index is not  N:
+         template1('axial',index)
+         template2('axial',index)
+         index++
 
-for a1 in range(x_dim):
-    for b1 in range(y_dim):     
-        for c1 in range(z_dim): 
-            if Ms[a1][b1][c1]==2:
-               if (template1('saggital',a1)['Bool'] or template2('saggital',a1)['Bool']) and (template1('saggital',b1)['Bool'] or template2('coronal',b1)['Bool'] is True:
-                  print 'Fiducial present at (x,y,z)'
-                  fidu_coordinates[x][y][z]=1
-                                                                              
 
-           
+        for a in range(x_dim):
+            for b in range(y_dim):
+                for c in range(z_dim):
+                    if Ms[a][b][c]==1:
+                       if template2('saggital',a) is True and template2('coronal',b) is True:
+                          print 'Fiducial present at (a,b,c)'
+                          fidu_coordinates[a][b][c]=1
+
+        for a1 in range(x_dim):
+            for b1 in range(y_dim):
+                for c1 in range(z_dim):
+                    if Ms[a1][b1][c1]==2:
+                       if (template1('saggital',a1) or template2('saggital',a1)) and (template1('saggital',b1) or template2('coronal',b1) is True:
+                         print 'Fiducial present at (a1,b1,c1)'
+                         fidu_coordinates[a1][b1][c1]=1
+
+return  fidu_coordinates      
 
 
 
