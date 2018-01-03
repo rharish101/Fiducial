0c478f751e23315a5d904a2b4f36352b10f61667
rharish101 <harish.rajagopals@gmail.com>
2018-01-03 20:19:07 +0530

    Added template detection

--- buffalo.py
+++ buffalo.py
@@ -6,21 +6,29 @@ from final_python import god_function
 from process import import_dicom
 import cv2
 
-folder = './2016.06.27 PVC Skull Model/Sequential Scan/DICOM/PA1/ST1/SE2/'
-images_axial = [] # orientation (-z, x, y)
-for img in os.listdir(folder):
-    images_axial.append(import_dicom(folder + img))
-
 # NOTE: Refer to https://en.wikipedia.org/wiki/Anatomical_plane for orientation
-temp = list(map(lambda img: cv2.resize(img, (176, 176)), images_axial))
+dir_axial = './2016.06.27 PVC Skull Model/Sequential Scan/DICOM/PA1/ST1/SE2/'
+images_axial = [] # orientation (-z, x, y)
+for img in sorted(os.listdir(dir_axial),
+                  key=lambda img_name: int(img_name[2:]))[3:-2]:
+    for _ in range(3):
+        images_axial.append(import_dicom(dir_axial + img))
+images_axial = np.array(images_axial[:-1])
 
 # orientation (-x, -y, -z)
-images_sagittal = np.array(map(lambda img: cv2.resize(img, (512, 512)),
-    np.swapaxes(np.swapaxes(temp, 0, 1), 1, 2)[::-1, ::-1, :]))
+images_sagittal = np.swapaxes(np.swapaxes(images_axial, 0, 1),
+                              1, 2)[::-1, ::-1, :]
 
 # orientation (-y, x, -z)
-images_coronal = np.array(map(lambda img: cv2.resize(img, (512, 512)),
-                              np.swapaxes(temp, 0, 2)[::-1]))
+images_coronal = np.swapaxes(images_axial, 0, 2)[::-1]
+
+#dir_coronal = './2016.06.27 PVC Skull Model/Spiral Scan/DICOM/PA1/ST1/SE6/'
+#images_coronal = []
+#for img in sorted(os.listdir(dir_coronal),
+                  #key=lambda img_name: int(img_name[2:]))[2:-1]:
+    #for _ in range(3):
+        #images_coronal.append(import_dicom(dir_coronal + img))
+#images_coronal = np.array(images_coronal[:-1])
 
 if __name__ == '__main__':
     print(god_function(images_axial, images_coronal, images_sagittal))
--- final_python.py
+++ final_python.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import numpy as np
+from template_0.slid_win import sliding_windows
+from process import shi_tomasi
 
-#enter the dimensions of the image
-x_dim=176
-y_dim=176
-z_dim=176
-
-Ma=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
-Mc=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
-Ms=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
-
-#def template1(plane,index):
 def template1(img):
-    #Boolean is true if image has template1 
-    #x,y are the coordinates of the center of concentric circles and sets Ma[x][y][index]=1 is plane='axial'....
-    return {'Bool': True,'x_coor': 0,'y_coor': 0}
+    return sliding_windows(img, './template_0/template_0_detect.h5', (50, 50),
+                           1)
 
-#def template2(plane,index):
 def template2(img):
     #x,y are the coordinates of the center of the valley
     #returns true if template2 is found and sets Ma[x][y][index]=2 if plane='axial'...    
-    return {'Bool': True,'x_coor': 0,'y_coor': 0}
+    return shi_tomasi(img)
 
 def god_function(list_axial,list_coronal,list_saggital): 
-        
-        fidu_coordinates=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
-        for index in range(1, 1 + len(list_axial)):
-         template1(list_axial[index - 1])
-         template2(list_axial[index - 1])
+    x_dim=176
+    y_dim=176
+    z_dim=176
 
+    Ma=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+    Mc=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+    Ms=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
 
-        for a in range(x_dim):
-            for b in range(y_dim):
-                for c in range(z_dim):
-                    if Ms[a][b][c]==1:
-                       if template2(list_saggital[a])['Bool'] is True and template2(list_coronal[b])['Bool'] is True:
-                          print('Fiducial present at (a,b,c)')
-                          fidu_coordinates[a][b][c]=1
+    fidu_coordinates=np.zeros((x_dim,y_dim,z_dim),dtype=np.float32)
+    for index in range(1, 1 + len(list_axial)):
+        template1(list_axial[index - 1])
+        template2(list_axial[index - 1])
 
-        for a1 in range(x_dim):
-            for b1 in range(y_dim):
-                for c1 in range(z_dim):
-                    if Ms[a1][b1][c1]==2:
-                       if (template1(list_saggital[a1])['Bool'] or template2(list_saggital[a1])['Bool']) and (template1(list_saggital[b1])['Bool'] or template2(list_coronal[b1])['Bool']):
-                         print('Fiducial present at (a1,b1,c1)')
-                         fidu_coordinates[a1][b1][c1]=1
+    for a in range(x_dim):
+        for b in range(y_dim):
+            for c in range(z_dim):
+                if Ms[a][b][c]==1:
+                    if template2(list_saggital[a])['Bool'] is True and template2(list_coronal[b])['Bool'] is True:
+                        print('Fiducial present at (a,b,c)')
+                        fidu_coordinates[a][b][c]=1
 
-        return  fidu_coordinates      
+    for a1 in range(x_dim):
+        for b1 in range(y_dim):
+            for c1 in range(z_dim):
+                if Ms[a1][b1][c1]==2:
+                    if (template1(list_saggital[a1])['Bool'] or template2(list_saggital[a1])['Bool']) and (template1(list_saggital[b1])['Bool'] or template2(list_coronal[b1])['Bool']):
+                        print('Fiducial present at (a1,b1,c1)')
+                        fidu_coordinates[a1][b1][c1]=1
+
+    return  fidu_coordinates      
 
 
 
