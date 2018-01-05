b5f66903a337449fab9b07297b4695721c527026
rharish101 <harish.rajagopals@gmail.com>
2018-01-05 20:30:11 +0530

    Fixed algo in code

--- final_python.py
+++ final_python.py
@@ -15,8 +15,6 @@ def template2(img):
 def god_function(list_axial, list_coronal, list_sagittal): 
     length = len(list_axial)
     Ma = np.zeros(list_axial.shape, dtype=np.uint8)
-    Mc = np.zeros(list_axial.shape, dtype=np.uint8)
-    Ms = np.zeros(list_axial.shape, dtype=np.uint8)
 
     fidu_coordinates = []
     for z in range(length):
@@ -24,28 +22,28 @@ def god_function(list_axial, list_coronal, list_sagittal):
             #if (length - z), y in template2(list_sagittal[x]) and\
             #(length - z), x in template2(list_coronal[y]):
                 #fidu_coordinates.append((x, y, z))
-            Ma[x, y, index] = 1
-        for y, x in template2(list_axial[index]):
+            Ma[x, y, z] = 1
+        for y, x in template2(list_axial[z]):
             #if ((length - z), y in template1(list_sagittal[x]) or\
             #(length - z), y in template2(list_sagittal[x])) and\
             #((length - z), x in template1(list_coronal[y]) or\
             #(length - z), x in template2(list_coronal[y])):
                 #fidu_coordinates.append((x, y, z))
-            Ma[x, y, index] = 2
+            Ma[x, y, z] = 2
 
-    for a in range(length):
-        for b in range(length):
-            for c in range(length):
-                if Ma[a][b][c] == 1 and\
-                len(template2(list_sagittal[a])) > 0 and\
-                len(template2(list_coronal[b])) > 0:
-                    fidu_coordinates.append((a, b, c))
-                elif Ma[a][b][c] == 2 and\
-                (len(template1(list_sagittal[a])) > 0 or\
-                len(template2(list_sagittal[a])) > 0) and\
-                (len(template1(list_coronal[a])) > 0 or\
-                len(template2(list_coronal[a])) > 0):
-                    fidu_coordinates.append((a, b, c))
+    for x in range(length):
+        for y in range(length):
+            for z in range(length):
+                if Ma[x][y][z] == 1 and\
+                (length - z), y in template2(list_sagittal[x]) and\
+                (length - z), x in template2(list_coronal[y]):
+                    fidu_coordinates.append((x, y, z))
+                elif Ma[x][y][z] == 2 and\
+                ((length - z), y in template1(list_sagittal[x]) or\
+                (length - z), y in template2(list_sagittal[x])) and\
+                ((length - z), x in template1(list_coronal[y]) or\
+                (length - z), x in template2(list_coronal[y])):
+                    fidu_coordinates.append((x, y, z))
 
     return fidu_coordinates
 
