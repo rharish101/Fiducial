f8a0ed6e4e0c54da40855ebe3a0abb4efcb362a5
rharish101 <harish.rajagopals@gmail.com>
2018-01-05 21:38:50 +0530

    Reduced complexity

--- final_python.py
+++ final_python.py
@@ -20,7 +20,8 @@ def god_function(list_axial, list_coronal, list_sagittal):
             #if (length - z), y in template2(list_sagittal[x]) and\
             #(length - z), x in template2(list_coronal[y]):
                 #fidu_coordinates.append((x, y, z))
-            Ma[x, y, z] = 1
+            if z == 0 or Ma[x, y, z - 1] == 0:
+                Ma[x, y, z] = 1
         for y, x in template2(list_axial[z]):
             #if ((length - z), y in template1(list_sagittal[x]) or\
             #(length - z), y in template2(list_sagittal[x])) and\
@@ -29,16 +30,6 @@ def god_function(list_axial, list_coronal, list_sagittal):
                 #fidu_coordinates.append((x, y, z))
             Ma[x, y, z] = 2
             
-    for x in range(length):
-        for x in range(length):
-            for z in range(length):
-                #if there is series of 1's then this code makes makes the 1's after the first 1 to be 0
-                    if Ma[x,y,z]==1:
-                    z+=1
-                    while(Ma[x,y,z]==1):
-                        Ma[x,y,z]=0
-                        z+=1
-
     for x in range(length):
         for y in range(length):
             for z in range(length):
