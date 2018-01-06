f3371e1e92661a1e060fa6046738d30d1b9827bc
rharish101 <harish.rajagopals@gmail.com>
2018-01-07 03:39:24 +0530

    Added functionality

--- block.py
+++ block.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import dicom
 import os
 import numpy as np
 from scipy.ndimage import gaussian_filter
+from hough_transform_new import hough
 
 folder = '/home/rharish/Programs/Python/Fiducial/2012.09.15 ACRELIC1 CT Scan '\
          'Data from ACTREC/09171700/'
 images_axial = []
+print("Importing DICOM...")
 for dicom_img in sorted(os.listdir(folder), key=lambda img: int(img)):
     dic = dicom.read_file(folder + dicom_img)
     if 'ORIGINAL' in dic.ImageType:
         images_axial.append(dic)
         images_axial.append(dic)
 
+print("Forming arrays...")
 pixel_spacing = list(map(float, images_axial[0].PixelSpacing))
 images_axial = np.array([np.uint8(np.where(dic.pixel_array > 255, 255,
     dic.pixel_array)) for dic in sorted(images_axial,
@@ -28,8 +32,10 @@ images_axial = np.array(list(map(lambda img: gaussian_filter(img, 1),
                                  images_axial)))
 
 if __name__ == '__main__':
-    xs, ys = zip(*hough(images_axial, images_coronal, images_sagittal))
+    print("Starting detection...")
+    xs, ys, zs = zip(*hough(images_axial, images_coronal, images_sagittal))
     xs = np.array(xs) * pixel_spacing[1]
-    ys = np.array(ys) * pixel_spacing[1]
-    print(zip(xs, ys))
+    ys = np.array(ys) * pixel_spacing[0]
+    zs = np.array(zs) * 512 * (pixel_spacing[0] / len(zs))
+    print(list(zip(xs, ys, zs)))
 
--- hough_transform_new.py
+++ hough_transform_new.py
@@ -4,22 +4,52 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from process import img_hist, func_minima, clahe_img, display, thresh_hist
+import sys
 
-img = cv2.imread('./acrelic1/tp_21.png',0)
-img = cv2.medianBlur(img,5)
-img=thresh_hist(img)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-#hyperparameters for acrelic 1
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,30,
-                            param1=50,param2=40,minRadius=10,maxRadius=0)
+def hough(images_axial, images_coronal, images_sagittal):
+    centers = []
+    for z, img in enumerate(images_axial):
+        img = cv2.medianBlur(img, 5)
+        img = thresh_hist(img)
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 30, param1=50,
+                                   param2=40, minRadius=10, maxRadius=0)
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-    # draw the center of the circle
-    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            centers.extend([(i[1], i[0], z) for i in circles[0, :]])
+        sys.stdout.write("\r" + str(z) + " images done out of " +\
+                         str(len(images_axial)) + "\r")
+        sys.stdout.flush()
+    for y, img in enumerate(images_coronal):
+        img = cv2.medianBlur(img, 5)
+        img = thresh_hist(img)
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 30, param1=50,
+                                   param2=40, minRadius=10, maxRadius=0)
 
-cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            centers.extend([(i[1], y, len(images_axial) - i[0]) for i in\
+                            circles[0, :]])
+        sys.stdout.write("\r" + str(y) + " images done out of " +\
+                         str(len(images_coronal)) + "\r")
+        sys.stdout.flush()
+    for x, img in enumerate(images_sagittal):
+        img = cv2.medianBlur(img, 5)
+        img = thresh_hist(img)
+        cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 30, param1=50,
+                                   param2=40, minRadius=10, maxRadius=0)
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            centers.extend([(x, i[1], len(images_axial) - i[0]) for i in\
+                            circles[0, :]])
+        sys.stdout.write("\r" + str(z) + " images done out of " +\
+                         str(len(images_sagittal)) + "\r")
+        sys.stdout.flush()
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+    return centers
+
