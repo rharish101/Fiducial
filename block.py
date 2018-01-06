9363455e61f7f2ac6762d9d81ac59fa036e65731
rharish101 <harish.rajagopals@gmail.com>
2018-01-07 01:40:13 +0530

    Added file for acrelic1 3d model

---
+++ block.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import dicom
+import os
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+folder = '/home/rharish/Programs/Python/Fiducial/2012.09.15 ACRELIC1 CT Scan '\
+         'Data from ACTREC/09171700/'
+images_axial = []
+for dicom_img in sorted(os.listdir(folder), key=lambda img: int(img)):
+    dic = dicom.read_file(folder + dicom_img)
+    if 'ORIGINAL' in dic.ImageType:
+        images_axial.append(dic)
+        images_axial.append(dic)
+images_axial = np.array([dic.pixel_array for dic in sorted(images_axial,
+    key=lambda dic: int(dic.SliceLocation))])
+
+images_sagittal = np.array(list(map(lambda img: gaussian_filter(img, 1),
+    np.swapaxes(np.swapaxes(images_axial, 0, 2), 1, 2)[:, ::-1, :])))
+
+images_coronal = np.array(list(map(lambda img: gaussian_filter(img, 1),
+    np.swapaxes(images_axial, 0, 1)[:, ::-1, :])))
+
+images_axial = np.array(list(map(lambda img: gaussian_filter(img, 1),
+                                 images_axial)))
+
